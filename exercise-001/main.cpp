6b6e2abdd34fced67bcf1f0a79d71d63b2e1d741
Alexander-v-B <149794239+Alexander-v-B@users.noreply.github.com>
2023-11-03 15:25:45 +0100

    new stack variable

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -11,20 +11,23 @@ int foo;
 
 // .bss segment
 int bss = 0;
-// .data Segment
+// .data segment
 int data = 4711;
 
-// .rodata Segment
+// .rodata segment
 const int rodata = 42;
 
 
 auto main(int argc, char **argv) -> int
 {
-   
+   // .stack segment
+    int stack = 12;
+
    // output lines
     fmt::print("Value of variable foo {}, address of variable foo {}\n", foo, fmt::ptr(&foo));
     fmt::print("Value of variable bss {}, address of variable bss {}\n", bss, fmt::ptr(&bss));
     fmt::print("Value of variable data {}, address of variable data {}\n", data, fmt::ptr(&data));
     fmt::print("Value of variable rodata {}, address of variable rodata {}\n", rodata, fmt::ptr(&rodata));
+    fmt::print("Value of variable stack {}, address of variable stack {}\n", stack, fmt::ptr(&stack));
     return 0; /* exit gracefully*/
 }
