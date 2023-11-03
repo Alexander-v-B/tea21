2720a79d7fe6a32ea3c90e350e7bfbf9ca46f49c
Alexander-v-B <149794239+Alexander-v-B@users.noreply.github.com>
2023-11-03 15:00:15 +0100

    Adress example

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -7,9 +7,11 @@
 #include "CLI/CLI.hpp"
 #include "config.h"
 
+int foo;
+
 auto main(int argc, char **argv) -> int
 {
    
-    fmt::print("Hello, {}!\n", "hello !");
+    fmt::print("Value of variable foo {}, address of variable foo {}\n", foo, fmt::ptr(&foo));
     return 0; /* exit gracefully*/
 }
