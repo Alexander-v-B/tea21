de18f22130aa4df5c63e88b24534b1b5a667deff
Alexander-v-B <149794239+Alexander-v-B@users.noreply.github.com>
2023-11-03 15:09:25 +0100

    different variable types in memory

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -9,9 +9,21 @@
 
 int foo;
 
+// .bss segment
+int bss = 0;
+// .data Segment
+int data = 4711;
+
+// .rodata Segment
+const int rodata = 42;
+
+
 auto main(int argc, char **argv) -> int
 {
    
     fmt::print("Value of variable foo {}, address of variable foo {}\n", foo, fmt::ptr(&foo));
+    fmt::print("Value of variable bss {}, address of variable bss {}\n", bss, fmt::ptr(&bss));
+    fmt::print("Value of variable data {}, address of variable data {}\n", data, fmt::ptr(&data));
+    fmt::print("Value of variable rodata {}, address of variable rodata {}\n", rodata, fmt::ptr(&rodata));
     return 0; /* exit gracefully*/
 }
